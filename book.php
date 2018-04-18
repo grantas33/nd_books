6fbc2357a15e175556a404a81003d213c7d1ce1b
Grantas Gadliauskas <grantas0612@gmail.com>
2018-04-18 21:11:00 +0300

    add book page

--- book.php
+++ book.php
@@ -11,16 +11,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT bookId, title FROM Books";
+$bookId = $_GET['id'];
+$sql = "SELECT Books.Authors.name, title, `year` FROM Books.Books 
+LEFT JOIN Books.Authors ON Books.Authors.authorId = Books.Books.authorId
+WHERE Books.Books.bookId = ".$bookId;
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<table><tr><th>ID</th><th>Title</th></tr>";
+    echo "<table><tr><th>Author</th><th>Title</th><th>Year</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr>
-                <td><a href=\"book.php?id=".$row["bookId"]."\">".$row["bookId"]."</td> 
+                <td>".$row["name"]."</td> 
                 <td>".$row["title"]."</td>
+                <td>".$row["year"]."</td>
               </tr>";
     }
     echo "</table>";
